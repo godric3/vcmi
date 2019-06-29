123add2c4dae7d9eda42d9fced5056dd0f9a5a69
Dydzio <blood990@gmail.com>
2019-06-29 11:15:04 +0200

    Try symlink

--- CI/mxe/before_install.sh
+++ CI/mxe/before_install.sh
@@ -37,3 +37,4 @@ fi # Disable
 # alias for CMake
 sudo mv /usr/bin/cmake /usr/bin/cmake.orig
 sudo ln -s /usr/lib/mxe/usr/bin/$MXE_TARGET-cmake /usr/bin/cmake
+sudo ln -s /usr/lib/mxe/usr/i686-w64-mingw32.static /usr/lib/mxe/usr/i686-w64-mingw32.shared
